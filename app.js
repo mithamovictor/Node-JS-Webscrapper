245340a15ca03235dda01eaa3af8c701bbc644a2
victor <vickarungaru@gmail.com>
2022-01-26 14:07:26 +0300

    inital scrapper

---
+++ app.js
@@ -0,0 +1,16 @@
+const request = require("request-promise")
+const cheerio = require("cheerio")
+const url = "https://www.bullion-rates.com/gold/INR/2007-1-history.htm"
+request(url, (error, response, html) => {
+  if( !error && response.statusCode == 200) {
+    const $ = cheerio.load(html)
+    const datarow = $(".HeaderRow")
+    const output = datarow.find("th").text()
+    $(".DataRow").each((i, data) => {
+        const item = $(data).text()
+        const item1 = $(data).text()
+        const item2 = $(data).text()
+        console.log(item, item1, item2)
+    })
+  }
+})
